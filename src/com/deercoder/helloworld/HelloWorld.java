2e54b238ef1d0a27d0e39d85768108624b66f378
Chang Liu <lllccc2009@gmail.com>
2015-11-08 01:50:28 -0500

    update srcs

--- src/com/deercoder/helloworld/HelloWorld.java
+++ src/com/deercoder/helloworld/HelloWorld.java
@@ -14,13 +14,16 @@ public class HelloWorld {
 
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
+		private static int count = 0;
 		
 		
 		@Override
 		public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
 				throws IOException {
+			count += 1;
 			// get the input string from the file
 			String line = value.toString();
+			System.out.println("line " + count +  ": " + line);
 			// treat each string and split by space by default
 			StringTokenizer tokenizer = new StringTokenizer(line);
 			// get words and do iteratively
